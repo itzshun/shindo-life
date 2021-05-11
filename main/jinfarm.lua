c9f4b54ec7ed062c75f6325b7ba4c1c639138942
itzshun <44308140+itzshun@users.noreply.github.com>
2021-05-10 22:39:44 -0300

    Update jinfarm.lua

--- main/jinfarm.lua
+++ main/jinfarm.lua
@@ -10,21 +10,21 @@ while getgenv().Jin.AutoEquip do wait()
             KeyState = "z"
             local A_1 = "key"
             local A_2 = KeyState
-            local Event = game:GetService("Workspace").ItzShun.combat.update
+            local Event = game:GetService("Player").LocalPlayer.combat.update
             Event:FireServer(A_1, A_2)
         end
         local FindChakra = game:GetService("Players").LocalPlayer.statz.mastery.chakra
-        if FindChakra.Value <= 6000 then
+        if FindChakra.Value == 6000 then
             return
         else
             local A_1 = "addstat"
             local A_2 = "chakra"
             local A_3 = getgenv().Jin.Chakra
-            local Event = game:GetService("Players").ItzShun.startevent
+            local Event = game:GetService("Players").LocalPlayer.startevent
             Event:FireServer(A_1, A_2, A_3)
         end
         local FindNin = game:GetService("Players").LocalPlayer.statz.mastery.ninjutsu
-        if FindNin.Value <= 6000 then
+        if FindNin.Value == 6000 then
             return
         else
             local A_1 = "addstat"
